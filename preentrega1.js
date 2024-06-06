5d96f53723a4b77ca4405b7f78a1f356e0b862e8
Regina Castagnoli <regina.castagnoli@gmail.com>
2024-06-06 08:49:32 -0300

    cambio de detalles consola

--- preentrega1.js
+++ preentrega1.js
@@ -5,18 +5,18 @@ function calcularCambio(monto, moneda){
 
     let cambio = 0;
 
-    if(moneda == "DOLAR" || moneda == "dolar"){
+    if(moneda == "DOLARES" || moneda == "dolares"){
         let cambio = monto / 896;
-        
+        return cambio
 
     }
-    else if (moneda == "EURO" || moneda == "euro"){
+    else if (moneda == "EUROS" || moneda == "euros"){
        let cambio = monto / 970;
-        
+        return cambio
 
 
     }
-    return cambio
+    
 }
 
 function datosCambio (monto , moneda){
@@ -24,9 +24,9 @@ function datosCambio (monto , moneda){
     let cambioFinal = calcularCambio( monto, moneda);
 
     console.log ( "Datos de la convertibilidad:");
-    console.log ( "Monto en peso ingresado: ", monto);
+    console.log ( "Monto ingresado: ", monto , "pesos");
     console.log ( "Cambio a: ", moneda);
-    console.log ( "Valor final: ", cambioFinal);
+    console.log ( "Valor final: ", cambioFinal,moneda);
     
 }
 
@@ -41,7 +41,7 @@ while(monto != "SALIR") {
     if( monto != "SALIR"){
 
         monto = parseFloat( monto);
-        let moneda = prompt( " Ingrese el tipo de moneda a la cual quiere pasar: DOLAR | EURO");
+        let moneda = prompt( " Ingrese el tipo de moneda a la cual quiere pasar: DOLARES | EUROS");
         datosCambio ( monto, moneda);
 
 }
